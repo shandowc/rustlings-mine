f4bfe7e62f5292c1ed613df93a1cff39a96c4849
Zhang Xiaodong <zhxiaod3@163.com>
2023-07-19 00:52:18 +0800

    chore: 77/95

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,15 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(cnt: i32) -> i32 {
+    if cnt <= 40 {
+        return 2 * cnt;
+    } else {
+        return cnt;
+    }
+}
 
 // Don't modify this function!
 #[test]
--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -14,10 +14,12 @@
 
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
+
+use std::fmt::Display;
 
 pub struct ReportCard {
-    pub grade: f32,
+    pub grade: Box<dyn Display>,
     pub student_name: String,
     pub student_age: u8,
 }
@@ -36,7 +38,7 @@ mod tests {
     #[test]
     fn generate_numeric_report_card() {
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: Box::new(2.1),
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
         };
@@ -50,7 +52,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: Box::new("A+"),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
